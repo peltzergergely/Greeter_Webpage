bd91265ab84f6043746f46d2312adcfa4205ab97
peltzergergely <peltzer.gergely@gmail.com>
2017-02-22 22:53:59 +0100

    Base Setup
    Started doing the core of the webpage focusing on css.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Greeter</title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="header">
+	<h1>GREETER</h1>
+	<h2>Ünnepi köszöntések - ha éppen semmi frappáns nem jut az eszedbe!</h2>
+	<div class="search_bar">
+	Keresés...
+	</div>
+	<div class="body_border">
+		ide jönnek az smsek egyszer
+	</div>
+</div>
+
+
+</body>
+</html>
+	
