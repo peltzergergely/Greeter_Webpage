4bc39374bb135b30f930e89cc5c847a4cb62b206
peltzergergely <peltzer.gergely@gmail.com>
2017-02-23 18:24:55 +0100

    Template works in progress
    Expanded further css and added js.

--- index.php
+++ index.php
@@ -1,23 +1,35 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Greeter</title>
-	<link href="https://fonts.googleapis.com/css?family=Roboto:100" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-<div class="header">
-	<h1>GREETER</h1>
-	<h2>Ünnepi köszöntések - ha éppen semmi frappáns nem jut az eszedbe!</h2>
-	<div class="search_bar">
-	Keresés...
-	</div>
-	<div class="body_border">
-		ide jönnek az smsek egyszer
-	</div>
-</div>
-
-
-</body>
+	<head>
+		<title>Greeter</title>
+		<link href="https://fonts.googleapis.com/css?family=Roboto:100" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+		<script type="text/javascript" src="script.js"></script>
+	</head>
+	<body>
+		<div class="header">
+			<h1>GREETER</h1>
+			<h2>Ünnepi köszöntések - ha éppen semmi frappáns nem jut az eszedbe!</h2>
+			<div class="search_bar">
+				Keresés...
+				<div class="search_icon">
+				</div>
+			</div>
+			<div class="filter_boxes">
+				<div class="filter_box">nulla</div>
+				<div class="filter_box">egy</div>
+				<div class="filter_box">ketto</div>
+				<div class="filter_box">harom</div>
+				<div class="filter_box">negy</div>
+			</div>
+			<div class="body_border">
+				<div class="sms">elsõ</div>
+				<div class="sms">második</div>
+				<div class="sms">harmadik</div>
+				<div class="sms">negyedik</div>
+			</div>
+		</div>
+	</body>
 </html>
 	
---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.search_icon').hover(function(){
+		$(this).toggleClass('search_icon_hover');
+	});
+	$('.sms').hover(function(){
+		$(this).toggleClass('sms_hover');
+	});
+
+});
